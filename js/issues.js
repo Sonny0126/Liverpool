3cdcaed0709dbe97a0ade165e520a599a3f00bf5
Son Seung Min <110909735+Sonny0126@users.noreply.github.com>
2025-06-01 16:09:17 +0900

    Add files via upload
    upload frimpong signing

--- js/issues.js
+++ js/issues.js
@@ -20,6 +20,21 @@ const issues = [
                 There wasn’t any doubt in my head that this is the place to be for me and my family. I’m one of Liverpool. Someone called me the other day an adopted Scouser – I’m really proud to hear these things, it gives me a great feeling.”`
             }
         ]
+    },
+    {
+        title: "New Signing: JeremyLiverpool agree signing of Jeremie Frimpong from Bayer Leverkusen Joins Liverpool",
+        details: [
+            {
+                image: "../image/frimpong_signing.jpg",
+                text: `Jeremy frimpong: "It went quite easy. Liverpool came and said they had interest, and obviously for me it was a no-brainer."
+
+                “For me, it was like, ‘Whatever you guys do, just get this done’, [speaking to] my agents: ‘Just get this done.’
+
+                “Liverpool fans, I’m going to give my all, my energy, my work-rate and hopefully we can win together, we celebrate together, get everything together.
+
+                “I’m just excited to be here. Thank you guys for accepting me and I won’t let you guys down and I’ll give you the energy that you guys want.”`
+            }
+        ]
     }
 ];
 
