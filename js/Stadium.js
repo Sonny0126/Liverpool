9e4f9a41479af87831f2fcd6bbb291bb2c530f62
Son Seung Min <sonsmmm@naver.com>
2025-07-18 19:02:50 +0900

    hide api key

--- js/Stadium.js
+++ js/Stadium.js
@@ -1,6 +1,5 @@
-
 // 구글 API 키를 변수에 저장
-const apiKey = "AIzaSyBolxyvNrDE1ZT9yy8DxS5pBPVSgyXMWDw"; // 여기에 API 키를 입력합니다.
+const apiKey = ""; // 여기에 API 키를 입력합니다.
 
 // 구글 맵 API 초기화 함수
 function initMap() {
@@ -9,21 +8,21 @@ function initMap() {
 
   // 지도 객체 생성
   const map = new google.maps.Map(document.getElementById("map"), {
-      center: anfieldLocation,
-      zoom: 15,
+    center: anfieldLocation,
+    zoom: 15,
   });
 
   // 마커 추가 (Anfield 위치)
   const marker = new google.maps.Marker({
-      position: anfieldLocation,
-      map: map,
-      title: "Anfield Stadium"
+    position: anfieldLocation,
+    map: map,
+    title: "Anfield Stadium",
   });
 }
 
 // 구글 API 로드
 function loadGoogleMaps() {
-  const script = document.createElement('script');
+  const script = document.createElement("script");
   // 동적으로 API 키를 삽입
   script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
   script.async = true;
